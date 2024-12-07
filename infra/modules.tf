65dddb98be48ac4b44e29059bd555cf748103983
Sou Yoshihara <sousquared@gmail.com>
2024-12-07 15:08:42 +0900

    :sparkles: Update codes.

--- infra/modules.tf
+++ infra/modules.tf
@@ -39,7 +39,7 @@ resource "google_cloud_run_v2_service" "hello_cloud_run" {
   deletion_protection = false  # 削除保護を無効にする (terraform destroy時に削除できるようにする)
 }
 
-# Cloud Runの未認証呼び出し許可policy (本番環境ではmembersに適切な値を設定すること)
+# Cloud Runの未認証呼び出し許可policy 
 data "google_iam_policy" "noauth" {
   binding {
     role = "roles/run.invoker"
@@ -69,41 +69,21 @@ resource "google_compute_region_network_endpoint_group" "hello_neg" {
   }
 }
 
-# Load Balancerのcloud armor policy
-resource "google_compute_security_policy" "hello_policy" {
-  name        = "hello-policy"
-  description = "Load Balancer用のcloud armor policy"
-  rule {
-    action   = "allow"
-    priority = 1000
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        # FIXME: your ip address
-        src_ip_ranges = ["IP_ADDRESS"]
-      }
-    }
-    description = "my home global ip address"
-  }
-  rule {
-    action   = "deny(403)"
-    priority = 2147483647
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = ["*"]
-      }
-    }
-    description = "default rule"
-  }
-  adaptive_protection_config {
-    layer_7_ddos_defense_config {
-      enable = true
-    }
-  }
+# IAPクライアントの設定
+resource "google_iap_client" "project_client" {
+  display_name = "Hello Cloud Run Client"
+  brand        = "projects/${var.project}/brands/${var.project_number}"
 }
 
-# load balancerのbackend service
+# IAPの設定
+resource "google_iap_web_backend_service_iam_binding" "binding" {
+  project = var.project
+  web_backend_service = google_compute_backend_service.hello_backend_service.name
+  role = "roles/iap.httpsResourceAccessor"
+  members = var.iap_members
+}
+
+# バックエンドサービスにIAPを有効化
 resource "google_compute_backend_service" "hello_backend_service" {
   name                  = "hello-backend-service"
   protocol              = "HTTP"
@@ -111,15 +91,37 @@ resource "google_compute_backend_service" "hello_backend_service" {
   timeout_sec           = 30
   load_balancing_scheme = "EXTERNAL_MANAGED"
 
-  # cloud armor policyを指定
-  security_policy = google_compute_security_policy.hello_policy.id
-
   backend {
     group = google_compute_region_network_endpoint_group.hello_neg.self_link
   }
+
+  iap {
+    oauth2_client_id     = google_iap_client.project_client.client_id
+    oauth2_client_secret = google_iap_client.project_client.secret
+    enabled              = true
+  }
 }
 
-# url map
+# IAPサービスアカウントの作成
+resource "google_project_service_identity" "iap_sa" {
+  provider = google-beta
+  project  = var.project
+  service  = "iap.googleapis.com"
+}
+
+# IAPサービスアカウントの作成を待ってから実行されるように依存関係を追加
+resource "google_cloud_run_v2_service_iam_member" "iap_invoker" {
+  name = google_cloud_run_v2_service.hello_cloud_run.name
+  location = var.region
+  project  = var.project
+  role     = "roles/run.invoker"
+  member   = google_project_service_identity.iap_sa.member
+
+  depends_on = [
+    google_project_service_identity.iap_sa
+  ]
+}
+
 resource "google_compute_url_map" "hello_url_map" {
   name        = "hello-lb"
   description = "load balancer用のlb"
@@ -132,18 +134,55 @@ resource "google_compute_url_map" "hello_url_map" {
   }
 }
 
-resource "google_compute_target_http_proxy" "hello_target_http_proxy" {
-  name    = "predictor-target-http-proxy"
-  url_map = google_compute_url_map.hello_url_map.id
+# HTTPSプロキシの設定
+resource "google_compute_target_https_proxy" "hello_target_https_proxy" {
+  name             = "hello-target-https-proxy"
+  url_map          = google_compute_url_map.hello_url_map.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.hello_ssl_cert.id]
 }
 
-# フロントエンドの設定(http)
-resource "google_compute_global_forwarding_rule" "hello_forwarding_rule_http" {
-  name                  = "hello-forwarding-rule-http"
-  description           = "load balancerのforwarding rule(http)"
+# マネージドSSL証明書の設定
+resource "google_compute_managed_ssl_certificate" "hello_ssl_cert" {
+  name = "hello-ssl-cert"
+  managed {
+    domains = [var.domain]
+  }
+}
+
+# フロントエンドの設定(https)
+resource "google_compute_global_forwarding_rule" "hello_forwarding_rule_https" {
+  name                  = "hello-forwarding-rule-https"
+  description           = "load balancerのforwarding rule(https)"
   load_balancing_scheme = "EXTERNAL_MANAGED"
-  target                = google_compute_target_http_proxy.hello_target_http_proxy.id
+  target                = google_compute_target_https_proxy.hello_target_https_proxy.id
   ip_address            = google_compute_global_address.hello_lb_ip.address
   ip_protocol           = "TCP"
-  port_range            = "80"
+  port_range            = "443"
+}
+
+
+# DNSレコードの設定
+resource "google_dns_record_set" "hello" {
+  name = "${var.domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = var.dns_managed_zone  # 変数を使用
+
+  rrdatas = [google_compute_global_address.hello_lb_ip.address]
+}
+
+resource "google_dns_record_set" "hello_cname" {
+  name = "_acme-challenge.${var.domain}."
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = var.dns_managed_zone  # 変数を使用
+
+  rrdatas = ["gv-${substr(var.domain, 0, 32)}.${var.domain}."]
+}
+
+# SSL証明書の状態を取得するためのデータソース
+data "google_compute_ssl_certificate" "hello_cert" {
+  name = google_compute_managed_ssl_certificate.hello_ssl_cert.name
 }
--- infra/output.tf
+++ infra/output.tf
@@ -1,4 +1,23 @@
-output "load_balancer_ip" {
-  value       = google_compute_global_address.hello_lb_ip.address
+output "domain" {
+  description = "デプロイされたアプリケーションのドメイン名"
+  value       = var.domain
+}
+
+output "lb_ip" {
   description = "ロードバランサーの静的IPアドレス"
+  value       = google_compute_global_address.hello_lb_ip.address
+}
+
+output "url" {
+  description = "アプリケーションのURL"
+  value       = "https://${var.domain}"
+}
+
+output "certificate_status" {
+  description = "SSL証明書のステータス"
+  value = {
+    id = data.google_compute_ssl_certificate.hello_cert.certificate_id
+    creation_timestamp = data.google_compute_ssl_certificate.hello_cert.creation_timestamp
+    expire_time = data.google_compute_ssl_certificate.hello_cert.expire_time
+  }
 }
